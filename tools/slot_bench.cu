de692155dd2a30f9382afe3be119376529b1928e
ethpred <144991674+ethpred@users.noreply.github.com>
2026-09-07 08:14:19 +0000

    slot bench: collective identifier

--- tools/slot_bench.cu
+++ tools/slot_bench.cu
@@ -144,6 +144,7 @@ int main(int argc,char **argv)
         config.connect_timeout_milli = 60000u;
         config.operation_timeout_milli = 30000u;
         config.control_port_base = 21000u + rank_index;
+        config.collective_identifier = UINT64_C(0x534c4f54424e4348);
         config.backend_module_path = dso;
         config.local_host = "bench";
         {
