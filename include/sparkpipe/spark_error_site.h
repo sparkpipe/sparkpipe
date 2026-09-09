7c553ae8f54ba368da6e3a642afe9e63165d0068
ethpred <144991674+ethpred@users.noreply.github.com>
2026-09-09 10:48:12 +0000

    SPARK_FAIL: rename macro parameter (token substitution collided with struct member)

--- include/sparkpipe/spark_error_site.h
+++ include/sparkpipe/spark_error_site.h
@@ -6,25 +6,25 @@
 
 typedef struct SparkErrorSiteRecord
 {
-	int32_t status;
+	int32_t error_status;
 	const char *file;
 	uint32_t line;
 } SparkErrorSiteRecord;
 
 extern _Thread_local SparkErrorSiteRecord spark_last_error_site;
 
-#define SPARK_ERR_REPORT(status) \
+#define SPARK_ERR_REPORT(error_status) \
 	((void)fprintf(stderr,"ERRSITE %s:%d status=%d\n", \
-		__FILE__,__LINE__,(int)(status)))
+		__FILE__,__LINE__,(int)(error_status)))
 
-#define SPARK_FAIL(status) \
+#define SPARK_FAIL(error_status) \
 	do { \
-		int32_t spark_status = (int32_t)(status); \
-		spark_last_error_site.status = spark_status; \
+		int32_t spark_fail_status = (int32_t)(error_status); \
+		spark_last_error_site.error_status = spark_fail_status; \
 		spark_last_error_site.file = __FILE__; \
 		spark_last_error_site.line = (uint32_t)__LINE__; \
-		SPARK_ERR_REPORT(spark_status); \
-		return spark_status; \
+		SPARK_ERR_REPORT(spark_fail_status); \
+		return spark_fail_status; \
 	} while (0)
 
 #endif
